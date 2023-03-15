9ed485a584b2cd369e03f8803ce9294534cc2233
Saki <sbasken@gmail.com>
2023-03-15 12:29:05 -0700

    complete class shoe

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size = 0, condition = "Used"):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+    
+    def set_size(self, size):
+        if type(size) == int :
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def get_size(self):
+        return self._size
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        setattr(self, "condition", "New")
+
+    size = property(get_size, set_size,)
